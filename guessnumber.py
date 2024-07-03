58416e578cd24ec5faabe9f25287f6d27ca6ae95
Martyna Grzybek <mgrzybek312@gmail.com>
2024-07-03 22:38:41 +0200

    game

--- guessnumber.py
+++ guessnumber.py
@@ -1,1 +1,20 @@
-ok
+import random
+
+
+def generator():
+    drawed = random.choice(range(1, 11))
+    return drawed
+
+
+def game(gen, your=0):
+    while your != gen:
+        your = int(input("Enter the number: "))
+        if your < gen:
+            print("Your number is smaller!")
+        elif your > gen:
+            print("Your number is greater!")
+    print("You did it!")
+
+
+if __name__ == "__main__":
+    game(generator())
